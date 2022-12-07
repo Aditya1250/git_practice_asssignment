890567f9cfb7f6d30b73f59ed066b097aba13417
Aditya Savita <adityasavita1250@gmail.com>
2022-12-07 22:35:52 +0530

    this is a prime number program

---
+++ prime.js
@@ -0,0 +1,18 @@
+
+
+function checkPrime(num){
+    let count=0;
+    for(let i=0; i<=num; i++){
+      if(num%i==0){
+        count++
+      }
+    }
+    if(count==2){
+      console.log(num,"is a Prime number. ")
+    }else{
+      console.log(num,"is not a Prime number");
+    }
+    
+  }
+  // calling the function ------------------------
+  checkPrime(5);
